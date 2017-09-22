e7f22b4e566f33244b577413336b58545fd9583d
mrkokoc <ilia.guskov@gmail.com>
2017-09-23 00:58:24 +0300

    API Authentication & Token

--- models/user.js
+++ models/user.js
@@ -27,7 +27,7 @@ module.exports.getUserById = function (id, callback) {
     User.findById(id, callback);
 };
 
-module.exports.getUserByName = function (username, callback) {
+module.exports.getUserByUsername = function (username, callback) {
     const query = {username: username};
     User.findOne(query, callback);
 };
@@ -35,9 +35,16 @@ module.exports.getUserByName = function (username, callback) {
 module.exports.addUser = function (newUser, callback) {
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if(err) throw err;
+            if (err) throw err;
             newUser.password = hash;
             newUser.save(callback);
         });
+    });
+};
+
+module.exports.comparePassword = function (candidatePassword, hash, callback) {
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+        if (err) throw err;
+        callback(null, isMatch);
     });
 };
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const config = require('../config/database');
 
 const User = require('../models/user');
 
@@ -15,7 +16,7 @@ router.post('/register', (req, res, next) => {
     });
 
     User.addUser(newUser, (err, user) => {
-        if(err){
+        if (err) {
             res.json({success: false, msg: 'Failed to register user'})
         } else {
             res.json({success: true, msg: 'User registered'})
@@ -25,12 +26,42 @@ router.post('/register', (req, res, next) => {
 
 // Authenticate
 router.post('/authenticate', (req, res, next) => {
-    return res.send('AUTHENTICATE');
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.getUserByUsername(username, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+            return res.json({success: false, msg: 'User not found'})
+        }
+
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+                const token = jwt.sign({data: user}, config.secret, { // {data: user}
+                    expiresIn: 604800 // 1 week
+                });
+
+                res.json({
+                    success: true,
+                    token: 'JWT ' + token, // space after 'JWT '
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email
+                    }
+                });
+            } else {
+                return res.json({success: false, msg: 'Wrong password'})
+            }
+        });
+    });
 });
 
 // Profile
-router.get('/profile', (req, res, next) => {
-    return res.send('PROFILE');
+router.get('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+    res.json({user: req.user});
 });
 
 module.exports = router;
