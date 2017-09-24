e14509fdb606fee016b217f1d1433fdf5d5379d2
mrkokoc <ilia.guskov@gmail.com>
2017-09-24 13:55:39 +0300

    Linked mLab db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://<eli>:<cocacola>@ds147964.mlab.com:47964/meanauthapp',
+    database: 'mongodb://eli:cocacola@ds147964.mlab.com:47964/meanauthapp',
     secret: 'yoursecret'
 };
